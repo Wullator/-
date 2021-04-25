47fe0a24fd4ddd828798f31fc7c5ea37a5202ac3
Wullator <65553617+Wullator@users.noreply.github.com>
2021-04-25 17:16:43 +0400

    Add files via upload

---
+++ РулетОЧКА.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from tkinter import messagebox
+from random import randint
+r = Tk()
+def vo():
+   gg = randint(1,28)
+   #print(gg)
+   if gg <= 14:
+      messagebox.showinfo("GUI Python", H.get() + " Выиграл")
+      #print("Дима ")
+   elif gg >= 15:
+      messagebox.showinfo("GUI Python", J.get() + " Выиграл")
+      #print("Дима gg")
+H = StringVar()
+J = StringVar()
+L1 = Label(r, text = "Первый пункт")
+L1.pack()
+g = Entry(textvariable=H)
+g.pack()
+L2 = Label(r, text = "Второй пункт")
+L2.pack()
+k = Entry(textvariable=J)
+k.pack()
+b = Button(r, text = "Выбрать!", command = vo)
+b.pack()
+r.mainloop()
